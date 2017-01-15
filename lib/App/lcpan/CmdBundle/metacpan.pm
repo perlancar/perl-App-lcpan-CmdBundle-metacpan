86451f862502de22b285a9850cd949ddd5e92b51
perlancar (@pc-home) <perlancar@gmail.com>
2017-01-15 07:09:19 +0700

    Initial commit

---
+++ lib/App/lcpan/CmdBundle/metacpan.pm
@@ -0,0 +1,19 @@
+package App::lcpan::CmdBundle::metacpan;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: More lcpan subcommands related to MetaCPAN
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<lcpan>
+
+L<https://metacpan.org>
